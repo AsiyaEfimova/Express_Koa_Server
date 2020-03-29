a2c1d8fdb20d8e6eb4bf4d5de3e5447c0dda2ed5
a.efimova <asi4ka.84@gmail.com>
2020-03-29 13:03:42 +0300

    добавила загрузку файлов

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,20 +1,5 @@
 const db = require('../db');
 
 module.exports.get = function (req, res) {
-    res.render('pages/admin', { msgskill: req.flash('skillSave') });
-}
-
-module.exports.post = function (req, res) {
-    console.log(req.body)
-    const newSkill = {
-        age: req.body.age,
-        concerts: req.body.concerts,
-        cities: req.body.cities,
-        years: req.body.years
-    };
-    db.get('skills')
-        .push(newSkill)
-        .write();
-    req.flash('skillSave', 'Skills was saved');
-    res.redirect('/admin');
+    res.render('pages/admin', { msgskill: req.flash('skillSave'), msgfile: req.flash('goodSave') });
 }
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const ctrlHome = require('../controllers/main')
-const ctrlAdmin = require('../controllers/admin')
 const ctrlLogin = require('../controllers/login')
+const ctrlAdmin = require('../controllers/admin')
+const ctrlSkill = require('../controllers/skills')
+const ctrlGoods = require('../controllers/goods')
 
 router.get('/', ctrlHome.get)
 router.post('/', ctrlHome.post)
@@ -12,6 +14,8 @@ router.get('/login', ctrlLogin.get)
 router.post('/login', ctrlLogin.post)
 
 router.get('/admin', ctrlAdmin.get)
-router.post('/admin/skills', ctrlAdmin.post)
+
+router.post('/admin/skills', ctrlSkill.post)
+router.post('/admin/upload', ctrlGoods.post)
 
 module.exports = router
